fddb24a79f9281ee5ee5494f71877502c589e286
Tristan_Sama <tristan.manier@etu.univ-nantes.fr>
2024-01-16 11:08:54 +0100

    Gif support and setup for new solutions

--- public/js/solutions.js
+++ public/js/solutions.js
@@ -19,7 +19,8 @@ async function load_solution_list() {
 
         let img = document.createElement('img');
         img.src = "data/solutions/" + solution.raw_name + "/" + solution.images[0];
-        img.style.maxWidth = "30%";
+        img.style.maxWidth = "75%";
+        img.style.maxHeight = "75%";
         solution_section.appendChild(img);
 
         solutions_list.appendChild(solution_section);
